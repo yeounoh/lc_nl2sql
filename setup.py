bec372ba055ac39dc1224252df925989759cbb47
yeounoh <yeounoh@google.com>
2024-09-11 13:26:31 -0700

    Fix dependency issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lc_nl2sql',
-    version='0.1.3',
+    version='0.1.2',
     description='Long Context NL2SQL',
     author='Yeounoh Chung',
     author_email='yeounoh@google.com',
@@ -12,7 +12,7 @@ setup(
         'google-ai-generativelanguage>=0.6.3', 'transformers==4.39.1',
         'evaluate==0.4.0', 'func-timeout==4.3.5', 'datasets>=2.14.6',
         'pydantic==1.10.11', 'pylint==3.0.2',
-        'scipy ==1.11.3', 'langchain-openai==0.1.12',
+        'scipy ==1.11.3',
     ],
     entry_points={
         'console_scripts': [
