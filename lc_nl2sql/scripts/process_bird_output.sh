fed99626a96e1056bd64762ab56c927a04ee407e
Yeounoh Chung <yeounoh@google.com>
2024-10-11 16:26:30 +0000

    Add output processing script for BIRD ex acc

---
+++ lc_nl2sql/scripts/process_bird_output.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+prefix="$1"
+directory="$2"
+
+if [ -z "$prefix" ] || [ -z "$directory" ]; then
+  echo "Error: Please provide a prefix and directory path as arguments."
+  exit 1
+fi
+
+for pred_sql in "$directory"/"$prefix"*; do
+  if [ -f "$pred_sql" ]; then
+    echo "Processing file: $pred_sql"
+
+    python lc_nl2sql/eval/evaluation_bird.py \
+      --predicted_sql_path "$pred_sql" \
+      --ground_truth_path lc_nl2sql/data/bird/dev/dev.sql \
+      --db_root_path lc_nl2sql/data/bird/dev/dev_databases/ \
+      --num_cpus 24 \
+      --etype exec \
+      --gt_tied_json_path lc_nl2sql/data/bird/dev/dev_tied_append.json \
+      --diff_json_path lc_nl2sql/data/bird/dev/dev.json
+
+  fi
+done
