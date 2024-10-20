92a8d40669caabea6e33a8d87adabc89498f1d08
Yeounoh Chung <yeounoh@google.com>
2024-10-20 03:04:04 +0000

    Add multi_choice experiment

--- lc_nl2sql/experiments/bird_use_hint.sh
+++ lc_nl2sql/experiments/bird_use_hint.sh
@@ -38,7 +38,6 @@ python lc_nl2sql/predict/count_token.py \
   --predicted_input_filename lc_nl2sql/data/example_text2sql_dev.json \
   --predicted_out_filename "lc_nl2sql/output/pred/token_count/bird_use_hint_no_rules_and_with_hint"
 
-
 echo "Running with no rules but no hints"
 python lc_nl2sql/data_process/sql_data_process.py \
   --input_data_path lc_nl2sql/data/bird/dev/dev.json \
