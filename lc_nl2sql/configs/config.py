018b7591174e5e7be52595a09793c8eb199805b6
Yeounoh Chung <yeounoh@google.com>
2025-01-01 19:14:31 +0000

    Update prompt sample

--- lc_nl2sql/configs/config.py
+++ lc_nl2sql/configs/config.py
@@ -129,25 +129,25 @@ Output the SQL query string ONLY.
 
 COLUMN_SELECTOR_TEMPLATE= """
 You are a SQLite SQL expert.
-Your task is to examine the provided database schema, understand the posed question, and use the hint to pinpoint the specific columns within tables that are essential for crafting a SQL query to answer the question.
-
+Your task is to examine the provided database schema, understand the posed question, and use the (optional) hint to pinpoint the specific columns within tables that are essential for crafting a SQL query to answer the question.
+----------------------------------
 Database Schema Overview:
 {DATABASE_SCHEMA}
-
+----------------------------------
 This schema offers an in-depth description of the database's architecture, detailing tables, columns, primary keys, foreign keys, and any pertinent information regarding relationships or constraints. Special attention should be given to the examples listed beside each column, as they directly hint at which columns are relevant to our query.
 
 For key phrases mentioned in the question, we have provided the most similar values within the columns denoted by "-- examples" in front of the corresponding column names. This is a critical hint to identify the columns that will be used in the SQL query.
-
+-----------------------------------
 Question:
 {QUESTION}
 
 Hint:
 {HINT}
-
-The hint aims to direct your focus towards the specific elements of the database schema that are crucial for answering the question effectively.
+------------------------------------
+The hint aims to direct your focus towards the specific elements of the database schema that are crucial for answering the question effectively. Note that Hint may not be present and optional.
 
 Task:
-Based on the database schema, question, and hint provided, your task is to identify all and only the columns that are essential for crafting a SQL query to answer the question.
+Based on the database schema, question, and (optional) hint, your task is to identify all and only the columns that are essential for crafting a SQL query to answer the question.
 For each of the selected columns, explain why exactly it is necessary for answering the question. Your reasoning should be concise and clear, demonstrating a logical connection between the columns and the question asked.
 
 Tip: If you are choosing a column for filtering a value within that column, make sure that column has the value as an example.
