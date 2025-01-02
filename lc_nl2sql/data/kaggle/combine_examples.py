65d1608aa3517a72efff0854f41f1ee799b7c160
root <root@yeounoh-gpu-vm-l4.us-central1-c.c.sysres-disagg-ml.internal>
2025-01-02 19:04:29 +0000

    Run Kaggle test data

--- lc_nl2sql/data/kaggle/combine_examples.py
+++ lc_nl2sql/data/kaggle/combine_examples.py
@@ -6,17 +6,24 @@ def combine_json_files(folder_path):
   for filename in os.listdir(folder_path):
     if filename.endswith(".json"):
       file_path = os.path.join(folder_path, filename)
-      with open(file_path, 'r') as f:
-        try:
-          data = json.load(f)
-          # Assuming each file contains a list of JSON objects
-          combined_data.extend(data)
-        except json.JSONDecodeError as e:
-          print(f"Error decoding JSON in file {filename}: {e}")
+      if '_test' in file_path:
+          with open(file_path, 'r') as f:
+            try:
+              data = json.load(f)
+              # Assuming each file contains a list of JSON objects
+              combined_data.extend(data)
+            except json.JSONDecodeError as e:
+              print(f"Error decoding JSON in file {filename}: {e}")
   return combined_data
 
 combined_data = combine_json_files('examples')
+print(f'Numb examples: {len(combined_data)}')
+gold_sql = []
 for idx, item in enumerate(combined_data):
     item['question_id'] = idx
+    gold_sql.append(item['query'])
 with open("dev.json", "w") as outfile:
   json.dump(combined_data, outfile, indent=4)
+with open("dev_gold.sql", "w") as outfile:
+  for sql in gold_sql:
+      outfile.write(sql + "\n")
