58caece28824578de6217a45fb4361086f40bdee
Yeounoh Chung <yeounoh@google.com>
2025-01-03 16:59:51 +0000

    Measure kaggle latency

--- lc_nl2sql/experiments/kaggle_benchmark.sh
+++ lc_nl2sql/experiments/kaggle_benchmark.sh
@@ -11,7 +11,6 @@ if [[ ! -f "$input_file_sk100" ]]; then
   --db_folder_path lc_nl2sql/data/kaggle/databases \
   --db_tbl_col_vals_file db_tbl_col_vals_kaggle.pickle \
   --num_col_values 10 \
-  --use_column_filtering 1 \
   --synthetic_examples 1 \
   --num_examples 100
   #--filtered_schema_file lc_nl2sql/data/kaggle/col_selection_schema_kaggle.csv \
@@ -26,7 +25,12 @@ python lc_nl2sql/predict/predict.py \
   --use_disambiguation 1 \
   --db_folder_path lc_nl2sql/data/kaggle/databases \
   --predicted_out_filename "lc_nl2sql/output/pred/kaggle_benchmark.sql"
-  
+
+python lc_nl2sql/predict/measure_latency.py \
+  --predicted_input_filename "$input_file_sk100" \
+  --use_self_correction 0 \
+  --predicted_out_filename "lc_nl2sql/output/pred/latency/kaggle_measure_latency"
+ 
 
 echo "Use Kaggle w/ gemini-1.5-flash"
 input_file_sk100="lc_nl2sql/data/kaggle_dev_example_synthetic_examples_100_flash.json"
@@ -39,7 +43,6 @@ if [[ ! -f "$input_file_sk100" ]]; then
   --db_folder_path lc_nl2sql/data/kaggle/databases \
   --db_tbl_col_vals_file db_tbl_col_vals_kaggle.pickle \
   --num_col_values 10 \
-  --use_column_filtering 1 \
   --synthetic_examples 1 \
   --use_flash 1 \
   --num_examples 100
@@ -56,3 +59,9 @@ python lc_nl2sql/predict/predict.py \
   --use_flash 1 \
   --db_folder_path lc_nl2sql/data/kaggle/databases \
   --predicted_out_filename "lc_nl2sql/output/pred/kaggle_benchmark_flash.sql"
+
+python lc_nl2sql/predict/measure_latency.py \
+  --predicted_input_filename "$input_file_sk100" \
+  --use_self_correction 0 \
+  --use_flash 1 \
+  --predicted_out_filename "lc_nl2sql/output/pred/latency/kaggle_measure_latency_flash"
