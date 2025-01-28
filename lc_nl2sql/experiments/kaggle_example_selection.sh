e3a99cf982961d2b2a3fcad7cf517555466b13d8
Yeounoh Chung <yeounoh@google.com>
2025-01-28 22:58:25 +0000

    Kaggle self correction

--- lc_nl2sql/experiments/kaggle_example_selection.sh
+++ lc_nl2sql/experiments/kaggle_example_selection.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+echo "Running with GT"
+  python lc_nl2sql/data_process/sql_data_process.py \
+  --input_data_path lc_nl2sql/data/kaggle/dev.json \
+  --input_table_path lc_nl2sql/data/kaggle/KaggleDBQA_tables.json \
+  --db_folder_path lc_nl2sql/data/kaggle/databases \
+  --source_type "kaggle" \
+  --db_tbl_col_vals_file db_tbl_col_vals_kaggle.pickle \
+  --example_pool_type train \
+  --example_selection_file lc_nl2sql/data/kaggle/similar_examples.json \
+  --inject_gt_example 1 \
+  --num_examples 1
+
+  python lc_nl2sql/predict/predict.py \
+  --predicted_input_filename lc_nl2sql/data/example_text2sql_dev.json \
+  --db_tbl_col_vals_file db_tbl_col_vals_kaggle.pickle \
+  --num_beams 1 \
+  --temperature 0.5 \
+  --db_folder_path lc_nl2sql/data/kaggle/databases \
+  --predicted_out_filename "lc_nl2sql/output/pred/kaggle_example_selection_train_gt_0"
+
+
 # Example selection experiments
 n_examples=(5 20 50 75 100 125 200)
 for k in "${n_examples[@]}"; do
